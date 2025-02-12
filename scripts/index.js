e1fc82ad5eaead49be01c811d4c830261c00112d
Xeniya_Z <xeniya.zavd@yandex.com>
2025-02-12 16:17:35 +0100

    Add functionality to create and delete cards

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,25 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.getElementById('card-template').content;
+
+const renderCard = (cardItem, deleteCallback) => {
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
+
+  card.querySelector('.card__image').src = cardItem.link;
+  card.querySelector('.card__title').textContent = cardItem.name;
+
+  const deleteButton = card.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', () => deleteCallback(card));
+
+  return card
+}
+
+const deleteCard = (card) => card.remove();
+
+const placesList = document.querySelector('.places__list');
+initialCards.forEach((item) => {
+  const card = renderCard(item, deleteCard);
+  placesList.append(card);
+})
